160847fb1f57277e8a84670d0d9bdb6b29faa5f6
huuphongnguyen <huuphong.nnguyen1507@gmail.com>
2017-09-24 16:18:55 +0700

    Updated

---
+++ HeyYouProject/HeyYouProject/Controllers/ForYouController.swift
@@ -0,0 +1,18 @@
+//
+//  ForYouController.swift
+//  HeyYouProject
+//
+//  Created by Nguyen Huu Phong on 9/24/17.
+//  Copyright © 2017 Nguyen Huu Phong. All rights reserved.
+//
+
+import UIKit
+import LBTAComponents
+
+class ForYouController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+    }
+}
